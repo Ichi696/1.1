11a79f7f8760a4586ff48b78e789bac5a5bffdf2
Ichi696 <41021954+Ichi696@users.noreply.github.com>
2018-07-09 21:28:34 +0300

    Add files via upload

---
+++ defSquad/Squad.py
@@ -0,0 +1,11 @@
+def sqrt():
+    import math
+    Storona = "b",
+    a = Storona[0]
+    Perimetr = int(a) * 4
+    Ploshad = int(a) ** 2
+    D = math.sqrt(int(a))
+    print("Сторона квадрата = " + (Storona[0]))
+    print("Периметр = " + str(Perimetr))
+    print("Площадь = " + str(Ploshad))
+    print("Диагональ квадрата = " + str(D))
